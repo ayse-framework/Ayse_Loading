afc135830dfd4c27ff85a982e10c42aa15e31d60
helmimarif <helmimarif11@gmail.com>
2022-12-21 09:40:10 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ayse_Loading
+
+Loading Screen for AyseFramework
+
+## Dependency:
+[Ayse Core](https://github.com/ayse-framework/Ayse_Core)
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+author "helmimarif"
+description "Loading Screen for AyseFramework"
+version "1.0"
+
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+files {
+    "index.html",
+    "style.css",
+	"bg.jpg"
+}
+loadscreen "index.html"
+
+dependency "Ayse_Core"
